b5ae9ebc2416d3e7d859333ead02f8d4a804bad3
Caio27816 <31149173+Caio27816@users.noreply.github.com>
2018-08-19 13:01:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+
 
 const client = new Discord.Client();
-client.prefix = config.prefix;
+
 client.login(process.env.BOT_TOKEN);
 
 client.on("ready", () => {
